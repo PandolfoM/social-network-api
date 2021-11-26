01e755beb94234258cec37df91ab422bf5234035
Matthew Pandolfo <matt@pandolfo.com>
2021-11-25 19:20:35 -0500

    friend ids

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,6 @@
 const router = require("express").Router();
 const userRoutes = require("./user-routes");
-const thoughtRoute = require("./thought-route");
 
-router.use("/thoughts", thoughtRoute);
 router.use("/users", userRoutes);
 
 module.exports = router;
